5081aa1d420d4177cb86f4b27ab31dcdf1c0ae88
Onur Baskirt <onur.baskirt@dnata.com>
2018-10-25 11:02:21 +0400

    Login tests simplified.

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,23 +1,16 @@
 package tests;
 
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginTests extends BaseTest {
 
-    //Test Data
     private String wrongUsername = "onur@swtestacademy.com";
     private String wrongPassword = "11122233444";
-
-    @BeforeMethod(description = "Method Level Setup!")
-    public void loginSetup() {
-        //Open N11 HomePage and Go to Login Page
-        page.loginPage().get();
-    }
 
     @Test (description="Invalid Login Scenario with wrong username and password.")
     public void invalidLoginTest_InvalidUserNameInvalidPassword () {
         page.loginPage()
+                .get()
                 .WhenILoginToN11(wrongUsername, wrongPassword)
                 .ThenIVerifyLoginPassword(("E-posta adresiniz veya şifreniz hatalı"));
     }
@@ -25,6 +18,7 @@ public class LoginTests extends BaseTest {
     @Test (description="Invalid Login Scenario with empty username and password.")
     public void invalidLoginTest_EmptyUserEmptyPassword ()  {
         page.loginPage()
+                .get()
                 .WhenILoginToN11("","")
                 .ThenIVerifyLoginUserName("Lütfen e-posta adresinizi girin.")
                 .ThenIVerifyLoginPassword("Bu alanın doldurulması zorunludur.");
